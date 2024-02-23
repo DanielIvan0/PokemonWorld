498638dfbc98319c1f264bd5bb6bccf196fb0e3a
Iván Salado Torres <ivan.salado@bettercloud.com>
2024-02-23 11:52:09 -0600

    first commit

---
+++ src/main/java/com/example/pokemonworldproject/PokemonWorldProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokemonworldproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonWorldProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PokemonWorldProjectApplication.class, args);
+  }
+
+}
